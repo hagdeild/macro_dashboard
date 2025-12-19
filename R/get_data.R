00b3b63fadbf11328979c19e0d803c7b365eb36a
Vidar Ingason <vidar@vr.is>
2025-12-19 11:16:51 +0000

    Vinnumarkaðurinn og laun

--- R/get_data.R
+++ R/get_data.R
@@ -5,6 +5,7 @@ library(tidyverse)
 library(zoo)
 library(tempdisagg)
 library(readxl)
+library(janitor)
 
 history_back <- "2015-01-01"
 
@@ -90,8 +91,33 @@ mannfjoldi_vinnufaer_monthly_tbl <-
     mannfjoldi = as.numeric(predict(monthly_vinnufaer_td))
   )
 
+# 1.1.3 QMM gagnasett Seðlabankans ----
+qmm_tbl <- read_excel("data/qmm.xlsx", skip = 1, sheet = 3) |>
+  janitor::clean_names()
 
-# 2.0.0 Landsframleiðsla og viðskiptajöfnuður ----
+qmm_tbl <- qmm_tbl |>
+  slice(-c(1:3)) |>
+  rename("date" = "x1") |>
+  mutate(
+    date = date(as.yearqtr(date)),
+    across(where(is.character), as.numeric)
+  )
+
+# 1.1.4 Vísitala neysluverðs ----
+vnv_tbl <-
+  read_csv2(
+    "https://px.hagstofa.is:443/pxis/sq/50d1a327-4780-42cf-9f89-57d7b9e4ff8b"
+  ) |>
+  clean_names() |>
+  fix_date() |>
+  select(-manudur) |>
+  set_names("vnv", "date") |>
+  mutate(vnv = vnv / 10)
+
+vnv_qrt_tbl <- qmm_tbl |>
+  select(date, cpi)
+
+# 2.0.0 ÞJÓÐHAG STÆRÐIR ----
 # Keðjutengt verðmæti, árstíðarleiðrétt
 
 gdp_comp_tbl <- read_csv2(
@@ -118,6 +144,14 @@ gdp_comp_tbl <- gdp_comp_tbl |>
   )
 
 data_ls$gdp_comp <- gdp_comp_tbl
+
+# Landsframleiðsla
+gdp_tbl <- gdp_comp_tbl |>
+  filter(skipting == "Verg Landsframleiðsla") |>
+  select(date, value) |>
+  rename("gdp" = "value")
+
+data_ls$gdp <- gdp_tbl
 
 # 2.1.0 Á mann ----
 # Aðeins landsframleiðsla og einkaneysla
@@ -147,11 +181,15 @@ vidskiptajofnudur_tbl <- vidskiptajofnudur_tbl |>
     an_gomlu = if_else(is.na(an_gomlu), 0, an_gomlu),
     vidskiptajofnudur = if_else(an_gomlu == 0, vidskiptajofnudur, an_gomlu)
   ) |>
-  select(date, vidskiptajofnudur)
+  select(date, vidskiptajofnudur) |>
+  left_join(gdp_tbl) |>
+  drop_na() |>
+  mutate(hlutfall = vidskiptajofnudur / gdp)
 
 data_ls$vidskiptajofnudur <- vidskiptajofnudur_tbl
 
-# 3.0.0 Vinnumarkaðurinn ----
+
+# 3.0.0 VINNUMARKAÐURINN ----
 
 # 3.1.0 Hlutfall starfandi ----
 starfandi_tbl <- read_csv2(
@@ -163,6 +201,7 @@ starfandi_tbl <- read_csv2(
   mutate(hlutfall_starfandi = starfandi / mannfjoldi) |>
   drop_na()
 
+data_ls$starfandi <- starfandi_tbl
 
 # 3.2.0 Atvinnuleysi ----
 # Nota ekki atvinnuleysi eins og Vinnumálastofnun reiknar.
@@ -170,8 +209,7 @@ starfandi_tbl <- read_csv2(
 # Laga til minnkað starfshlutfall, fæ þetta sem hlutfall og
 # nota svo á mína útreikninga
 
-# 3.2.1 Lagfæring vegna minnkaðst starfshlutfalls ----
-
+# Minnkað starfshlutfall
 minnkad_starfshlutfall_tbl <- tibble::tibble(
   date = as.Date(c(
     "2020-04-01",
@@ -207,6 +245,9 @@ minnkad_starfshlutfall_tbl <- tibble::tibble(
   )
 )
 
+
+# 3.2.1 Lagfæring vegna minnkaðst starfshlutfalls ----
+
 vmst_unemp_tbl <- read_excel(
   "data/vmst.xlsm",
   sheet = "G2",
@@ -225,7 +266,7 @@ vmst_unemp_tbl <- read_excel(
   select(date, value)
 
 # bæti við minnkuðu
-vmst_unemp_tbl <- vmst_unemp_tbl |>
+minnkad_ratio_tbl <- vmst_unemp_tbl |>
   left_join(minnkad_starfshlutfall_tbl) |>
   mutate(
     minnkad_starfshlutfall = if_else(
@@ -262,7 +303,7 @@ atvinnuleysi_tbl <- read_excel(
 
 # laga minnkað starfshlutfall
 atvinnuleysi_tbl <- atvinnuleysi_tbl |>
-  left_join(vmst_unemp_tbl) |>
+  left_join(minnkad_ratio_tbl) |>
   mutate(atvinnuleysi = atvinnuleysi * minnkad_ratio) |>
   select(date, atvinnuleysi)
 
@@ -293,4 +334,141 @@ atvinnuleysi_langtima_tbl <- atvinnuleysi_lengd_tbl |>
   ) |>
   pivot_longer(cols = -date) |>
   filter(str_detect(name, "\\+12")) |>
-  select(date, value)
+  select(date, value) |>
+  left_join(mannfjoldi_monthly_tbl) |>
+  mutate(atvinnuleysi = value / mannfjoldi) |>
+  drop_na() |>
+  left_join(minnkad_ratio_tbl) |>
+  mutate(atvinnuleysi = atvinnuleysi * minnkad_ratio) |>
+  select(date, atvinnuleysi)
+
+data_ls$atvinnuleysi_langtima <- atvinnuleysi_langtima_tbl
+
+# 3.2.4 Laus störf ----
+laus_storf_tbl <-
+  read_csv2(
+    "https://px.hagstofa.is:443/pxis/sq/c57c066c-dea4-4e3f-b816-cb6b95678a9d"
+  ) |>
+  select(-c(2, 5)) |>
+  set_names("date", "fjoldi_starfa", "laus_storf") |>
+  mutate(
+    date = str_replace(date, "Á", "Q"),
+    date = date(as.yearqtr(date))
+  )
+
+data_ls$laus_storf <- laus_storf_tbl
+
+# 3.2.5 Vinnulitlir ----
+vinnulitlir_tbl <-
+  read_csv2(
+    "https://px.hagstofa.is:443/pxis/sq/570ecedc-2d61-48f0-8723-0c448065b94d"
+  ) |>
+  janitor::clean_names() |>
+  select(arsfjordungur, eining, contains("mannfjoldi")) |>
+  set_names("date", "eining", "fjoldi") |>
+  pivot_wider(names_from = eining, values_from = fjoldi) |>
+  pivot_longer(cols = -c(date, contains("Mannfjöldi"))) |>
+  set_names("date", "mannfjoldi", "eining", "value")
+
+vinnulitlir_tbl <- vinnulitlir_tbl |>
+  mutate(
+    hlutfall = value / mannfjoldi,
+    date = date(as.yearqtr(str_replace(date, "Á", "Q")))
+  )
+
+data_ls$vinnulitlir <- vinnulitlir_tbl
+
+# 3.2.5 Framleiðni ----
+framleidni_tbl <- qmm_tbl |>
+  select(date, prod) |>
+  drop_na()
+
+data_ls$framleidni <- framleidni_tbl
+
+# 4.0.0 LAUN ----
+
+# 4.1.0 Launavísitala Hagstofu ----
+laun_hagstofa_tbl <-
+  read_csv2(
+    "https://px.hagstofa.is:443/pxis/sq/a98a5289-5a3c-402e-b345-fc0678bd3962"
+  ) |>
+  set_names("date", "launavisitala") |>
+  mutate(date = make_date(str_sub(date, 1, 4), str_sub(date, 6, 7)))
+
+data_ls$launavisitala <- laun_hagstofa_tbl
+
+# 4.2.0 Laun starfsstétt ----
+laun_starfstett_tbl <-
+  read_csv2(
+    "https://px.hagstofa.is:443/pxis/sq/ba7c3a6f-685a-4897-8ccc-7fd9c3859ece"
+  ) |>
+  janitor::clean_names() |>
+  fix_date() |>
+  select(date, starfsstett, contains("visitolur")) |>
+  set_names("date", "starfsstett", "visitala") |>
+  arrange(date, starfsstett) |>
+  group_by(starfsstett) |>
+  mutate(visitala = visitala / visitala[1] * 100) |>
+  ungroup()
+
+data_ls$laun_starfsstett <- laun_starfstett_tbl
+
+# 4.3.0 Launþegahópar ----
+laun_hopar_tbl <-
+  read_csv2(
+    "https://px.hagstofa.is:443/pxis/sq/10a91d93-f14f-42d3-9c35-c5f6800d077c"
+  ) |>
+  clean_names() |>
+  fix_date() |>
+  select(date, launthegahopur, contains("visitolur")) |>
+  set_names("date", "hopur", "visitala") |>
+  arrange(date, hopur) |>
+  group_by(hopur) |>
+  mutate(visitala = visitala / visitala[1] * 100) |>
+  ungroup()
+
+data_ls$laun_hopur <- laun_hopar_tbl
+
+
+# 4.4.0 Atvinnugrein ----
+laun_atvinnugrein_tbl <-
+  read_csv2(
+    "https://px.hagstofa.is:443/pxis/sq/90f7a12f-a610-4135-9bbd-81b0e0310902"
+  ) |>
+  clean_names() |>
+  fix_date() |>
+  select(date, atvinnugrein, contains("visitolur")) |>
+  set_names("date", "atvinnugrein", "visitala") |>
+  mutate(
+    visitala = as.numeric(visitala),
+    atvinnugrein = str_remove(atvinnugrein, "\\s*\\([^)]+\\)$")
+  ) |>
+  arrange(date, atvinnugrein) |>
+  group_by(atvinnugrein) |>
+  mutate(visitala = visitala / visitala[1] * 100) |>
+  ungroup() |>
+  drop_na()
+
+data_ls$laun_atvinnugrein <- laun_atvinnugrein_tbl
+
+# 4.5.0 Ráðstöfunartekjur ----
+radstofunartekjur_tbl <-
+  read_csv2(
+    "https://px.hagstofa.is:443/pxis/sq/26e59868-9cb6-4272-be9e-338502460692",
+    na = ".."
+  ) |>
+  drop_na()
+
+radstofunartekjur_tbl <- radstofunartekjur_tbl |>
+  set_names("date", "value") |>
+  mutate(
+    date = date(as.yearqtr(str_replace(date, "Á", "Q")))
+  ) |>
+  left_join(vnv_qrt_tbl) |>
+  mutate(value = value / cpi) |>
+  select(-cpi) |>
+  mutate(value = value / value[1] * 100)
+
+data_ls$radstofunartekjur <- radstofunartekjur_tbl
+
+# 5.0.0 HÚSNÆÐISMARKAÐURINN ----
