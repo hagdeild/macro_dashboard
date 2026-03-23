4a8a0116408bc0724d5864d600d7705783f26396
Vidar <vidar@vr.is>
2026-03-23 09:35:21 +0000

    Update task

--- R/taskscheduler.R
+++ R/taskscheduler.R
@@ -1,22 +1,24 @@
 # Schedule tasks
 
-library(vr)
+library(taskscheduleR)
 
 
 # Macro data daily update
-schedule_r_script(
-  rscript = "R/macro_data_update.R",
+taskscheduler_create(
   taskname = "macro_data_update",
+  rscript = "c:/Users/vidar/Documents/Rwd/macro_dashboard/R/macro_data_update.R",
   schedule = "DAILY",
-  starttime = "20:00"
+  starttime = "20:00",
+  format(Sys.Date(), "%d.%m.%Y")
 )
 
 
 # weekly update of dashbaord main data prep
-schedule_r_script(
-  rscript = "R/get_data.R",
+taskscheduler_create(
   taskname = "macro_full_data",
+  rscript = "c:/Users/vidar/Documents/Rwd/macro_dashboard/R/get_data.R",
   schedule = "WEEKLY",
-  day      = "MON",
-  starttime = "09:05"
+  starttime = "09:05",
+  days = "MON"
 )
+
